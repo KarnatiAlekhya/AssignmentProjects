24a6c58194c47640ee1de55f834a9d5c2bd4ee6a
KarnatiAlekhya <109645697+KarnatiAlekhya@users.noreply.github.com>
2022-10-20 18:26:25 +0530

    Add files via upload

---
+++ capstone/backend-app/src/test/java/com/MyAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
